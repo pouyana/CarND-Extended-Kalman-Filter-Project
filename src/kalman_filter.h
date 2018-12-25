cd77d426af29a489000054bcb9ab5078cb4b5541
Pouyan Azari <pouyan.azari@uni-wuerzburg.de>
2018-12-25 16:34:45 +0100

    Working version

--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -28,7 +28,6 @@ public:
   void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
             Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
 
-  //float NormalizeAngle(float alpha);
   /**
    * Prediction Predicts the state and the state covariance
    * using the process model
